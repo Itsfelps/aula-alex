a645da52401f0f4f383daac8124b4f9102f52510
Itsfelps <128159625+Itsfelps@users.noreply.github.com>
2024-03-22 10:37:00 -0300

    Add files via upload

---
+++ lista-duas.js
@@ -0,0 +1,7 @@
+const alunos = ['Joao', 'Juliana', 'Ana', 'Caio'];
+const media = [10,8,7.5,9];
+
+const listaDealunosemedias = [alunos, medias];
+
+console.log(`A aluna da posiçao 1 da lista de alunos é: ${listaDealunosemedias[0][1]}.`
+);
